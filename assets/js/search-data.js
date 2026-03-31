5d22ab9ad83ba274fc9ecf0ea8f2c98dbf0f4ac2
tiffanyta1402 <125527467+tiffanyta1402@users.noreply.github.com>
2026-03-31 11:57:53 +0000

    Deploying to gh-pages from @ tiffanyta1402/tiffanyta1402.github.io@3ea35cf984549eb90d2f64ea68cd3530076be269 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -435,12 +435,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-won-best-poster-at-the-antimicrobial-resistance-genomes-big-data-and-emerging-technologies-at-wellcome-genome-campus-hinxton-england-trophy",
+      },{id: "news-won-best-poster-at-the-antimicrobial-resistance-genomes-big-data-and-emerging-technologies-at-wellcome-genome-campus-hinxton-england-trophy",
           title: 'Won best poster at the Antimicrobial Resistance – Genomes, Big Data and Emerging...',
           description: "",
           section: "News",},{id: "news-attended-the-amr-genomics-hackathon-at-jesus-college-cambridge-university-england-see-some-of-the-work-that-i-contributed-here",
@@ -535,13 +530,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
